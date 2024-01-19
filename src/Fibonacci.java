88e9518efdc47d3bf927cca381abed46251342b8
Ryan <ryanreynaldi5758@gmail.com>
2024-01-19 20:30:06 +0700

    optimized fibonacci

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -9,46 +9,59 @@ import java.util.Map;
  */
 public class Fibonacci {
     public static void main(String[] args) {
-        int input = 10;
-//        showFib(input);
-//        System.out.println();
-        showFibonacci(input);
+        int input = 100;
+        dpFibonacci(input);
     }
-    private static int fib(int n){
-        if(n<=1){
-            return n;
+
+//    //Base
+//    private static int fib(int n){
+//        if(n<=1){
+//            return n;
+//        }
+//        return fib(n-1)+fib(n-2);
+//    }
+//
+//    public static void showFib(int n){
+//        for (int i = 0; i < n; i++) {
+//            System.out.print(fib(i)+" ");
+//        }
+//    }
+//
+//    //Memoizated
+//    Map<Integer,Integer> cache = new HashMap<>();
+//    public int fibOptimized(int n){
+//        if(cache.containsKey(n)){
+//            return cache.get(n);
+//        }else {
+//            if(n<=1){
+//                return n;
+//            }else{
+//                int result = fibOptimized(n-1) + fibOptimized(n-2);
+//                cache.put(n,result);
+//                return result;
+//            }
+//        }
+//    }
+//
+//    public static void showFibonacci(int n){
+//        Fibonacci fibonacci = new Fibonacci();
+//        for (int i = 0; i < n; i++) {
+//            System.out.print(fibonacci.fibOptimized(i)+" ");
+//        }
+//    }
+
+    //TC = O(n)
+    //SC = O(n)
+    public static void dpFibonacci(int n){
+        int[] fib = new int[n];
+        fib[0] = 0;
+        fib[1] = 1;
+
+        for (int i = 2; i < n ; i++){
+            fib[i]=fib[i-1]+fib[i-2];
         }
-        return fib(n-1)+fib(n-2);
-    }
-
-    public static void showFib(int n){
-        for (int i = 0; i < n; i++) {
-            System.out.print(fib(i)+" ");
+        for (int i = 0; i < fib.length; i++) {
+            System.out.print(fib[i] + " ");
         }
     }
-
-    Map<Integer,Integer> cache = new HashMap<>();
-    public int fibOptimized(int n){
-        if(cache.containsKey(n)){
-            return cache.get(n);
-        }else {
-            if(n<=1){
-                return n;
-            }else{
-                int result = fibOptimized(n-1) + fibOptimized(n-2);
-                cache.put(n,result);
-                return result;
-            }
-        }
-    }
-
-    public static void showFibonacci(int n){
-        Fibonacci fibonacci = new Fibonacci();
-        for (int i = 0; i < n; i++) {
-            System.out.print(fibonacci.fibOptimized(i)+" ");
-        }
-    }
-
-
-
 }
