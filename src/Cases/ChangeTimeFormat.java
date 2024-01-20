0c6869cee22631f1501da09c8016e650c500e064
Ryan <ryanreynaldi5758@gmail.com>
2024-01-20 22:34:12 +0700

    add changetimeformat

---
+++ src/Cases/ChangeTimeFormat.java
@@ -0,0 +1,25 @@
+package Cases;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+import static Util.Input.inputString;
+
+public class ChangeTimeFormat {
+    public static void main(String[] args) {
+        String input = inputString();
+        changeTimeFormat(input);
+    }
+
+    private static String caseChangeTimeFormat(String input){
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("hh:mm:ss a");
+        LocalTime localTime = LocalTime.parse(input, inputFormatter);
+
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        return localTime.format(outputFormatter);
+    }
+
+    public static void changeTimeFormat(String input){
+        System.out.println(caseChangeTimeFormat(input));
+    }
+}
